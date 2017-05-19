a2bc4a14fa290b2a967ff27bcbc0cd23578b0b5e
wt12312345 <wt12312345@DESKTOP-Q69GDM6>
2017-05-19 22:16:27 +0800

    2

--- wtbase/src/main/java/com/wt/agent/dao/StatVisitDao.java
+++
@@ -1,8 +0,0 @@
-package com.wt.agent.dao;
-
-
-import com.wt.agent.entity.StatVisit;
-import com.wt.web.dao.AbstractDao;
-
-public interface StatVisitDao extends AbstractDao<StatVisit> {
-}
--- wtbase/src/main/java/com/wt/agent/dao/VisitAgentDao.java
+++
@@ -1,7 +0,0 @@
-package com.wt.agent.dao;
-
-import com.wt.agent.entity.VisitAgent;
-import com.wt.web.dao.AbstractDao;
-
-public interface VisitAgentDao extends AbstractDao<VisitAgent> {
-}
